987f8f787734207eb557c33e4ae8c26bdcf6b98c
Yashaswini C <115557172+Yashaswini2004@users.noreply.github.com>
2025-08-02 14:58:00 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import "./styles.css";
+import Player from "./component/Player";
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Player />
+      <div id="challenges"></div>
+    </>
   );
 }
+
+export default App;
