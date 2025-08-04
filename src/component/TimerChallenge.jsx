e35c61b78e62eecc6ebf97b317f53027f77bd123
Yashaswini C <115557172+Yashaswini2004@users.noreply.github.com>
2025-08-04 12:50:47 +0000

    start and stop timer

--- src/component/TimerChallenge.jsx
+++ src/component/TimerChallenge.jsx
@@ -1,14 +1,32 @@
+import { useState, useRef } from "react";
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  const [timerOut, setTimerOut] = useState(false);
+  const [timerStart, setTimerStart] = useState(false);
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerOut(true);
+    }, targetTime * 1000);
+    setTimerStart(true);
+  }
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerOut && <p>You lost!!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : " "}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={timerStart ? handleStop : handleStart}>
+          {timerStart ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is running...</p>
+      <p className={timerStart ? "active" : undefined}>
+        {timerStart ? "Time is running..." : "Timer is inactive"}
+      </p>
     </section>
   );
 }
